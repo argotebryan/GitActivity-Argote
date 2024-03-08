1785f8910bce9751615f0b601fef2669ffcfa8c6
argotebryan <159995158+argotebryan@users.noreply.github.com>
2024-03-08 11:31:13 +0800

    Create git-python-argote.py

---
+++ GitActivity-Argote-Bryan /git-python-magno.py
@@ -0,0 +1,8 @@
+# GitActivity-Argote
+print("Bryan L, Argote")
+print("Brgy. Sibuyao, Trrijos, Marinduque")
+print("April 12, 2003")
+
+print("Clloge if Information and Computing Sciences")
+print("Bs Information Technology")
+print("2-C")
